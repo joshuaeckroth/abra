0f0b3e05f390ba15052d63bc2bb6241e7a1e55f6
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2014-06-14 22:13:30 -0400

    Added batch testing for ilnux example.

--- examples/linux/scripts.lisp
+++ examples/linux/scripts.lisp
@@ -11,3 +11,16 @@
 (defun test-all-exp-cases (steps &optional (pb #'pick-belief-fewrules-unattached) (print? nil))
   (test-exp-cases steps *cases* *rules* pb nil print?))
 
+(defun test-linux (repetitions)
+  (format t "PickBelief,Steps,Precision,Recall,PartialAccuracy~%")
+  (dolist (pb '("pick-belief-random" "pick-belief-fewrules-unattached" "pick-belief-recency-unattached"))
+    (dolist (steps '(100 250 500 1000 1500 2000))
+      (loop for i from 1 to repetitions
+         do (clearmem)
+           (load-problem "linux/scripts")
+           (load-problem "linux/kb")
+           (load-problem "linux/evidence")
+           (load-problem "linux/cases")
+           (destructuring-bind (prec recall gt-pm-overall)
+               (test-all-exp-cases steps (symbol-function (find-symbol (string-upcase pb) :abra)))
+             (format t "~A,~A,~A,~A,~A~%" pb steps prec recall gt-pm-overall))))))
